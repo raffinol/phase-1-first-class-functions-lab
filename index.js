aa68838b4d3a874f5430c6550010c58c0c14d46d
Rafael Finol <rafaelfinol88@gmail.com>
2023-03-27 01:32:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0,2);
+console.log(returnFirstTwoDrivers(drivers));
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2)
+console.log(returnLastTwoDrivers(drivers));
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(i){
+    return multiplier = fare => fare * i;   
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func){
+    return func(drivers);
+}
